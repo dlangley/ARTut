96b07ed1a7c8a95f4f905e992cfb3df080256022
Dwayne Langley <Dwayne.Langley@gmail.com>
2017-10-16 10:05:36 -0700

    Initial Commit

---
+++ ARTut/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARTut
+//
+//  Created by Dwayne Langley on 10/16/17.
+//  Copyright © 2017 Dwayne Langley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
